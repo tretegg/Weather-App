e4d577ce66b33ed9880351cc02f8fe6855d6ee26
tretegg <73811002+tretegg@users.noreply.github.com>
2024-12-09 22:59:33 +0000

    tried to add mine generation

--- src/routes/minesweeper/Board.svelte
+++ src/routes/minesweeper/Board.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { onMount } from "svelte";
 	import Cell from "./Cell.svelte";
 
 	// Props
@@ -6,7 +7,32 @@
 
 	// Static board for UI demonstration
 	const BOARD_SIZE = 8;
-	const board = Array(BOARD_SIZE).fill(Array(BOARD_SIZE).fill(0));
+	let  board = Array(BOARD_SIZE).fill(Array(BOARD_SIZE).fill(0));
+	const MINE_COUNT = 10;
+
+	onMount(() => {
+		generateMines();
+		console.table(board)
+	});
+
+	function generateMines () {
+		for (let i = 0; i < MINE_COUNT; i++) {
+			let done = false;
+			while (!done) {
+				let x = getRandomInt(8);
+				let y = getRandomInt(8);
+
+				if (board[x][y] != 1) {
+					board[x][y] = 1;
+					done = true;
+				} 
+			}
+		}
+	}
+
+	function getRandomInt(max: number) {
+		return Math.floor(Math.random() * max);
+	}
 </script>
 
 <div class="grid grid-cols-8 gap-1 gap-y-3 bg-transparent rounded">
